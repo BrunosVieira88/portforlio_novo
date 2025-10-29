39a9b6ddac7c276384a6a88d6fdc8d43a2915808
Bruno Vieira <brunosvieira88@gmail.com>
2025-10-28 21:55:56 -0300

    ajuste bootstrap

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,17 +4,23 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="css/all.min.css" />
-    <link rel="stylesheet" href="css/font.awesome.min.css" />
-    <script src="css/all.min.js"></script>
-    <script src="js/font.awesome.min.js"></script>
-    <link rel="stylesheet" href="{{asset('site/bootstrap.css')}}">
-    
-    <title>{{ $title }}</title>
+
+    <!-- FONT AWESOME -->
+    <link rel="stylesheet" href="{{ asset('site/all.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('site/fontawesome.min.css') }}">
+    <script src="{{ asset('site/all.min.js') }}"></script>
+    <script src="{{ asset('site/fontawesome.min.js') }}"></script>
+
+    <!-- BOOTSTRAP -->
+    <link rel="stylesheet" href="{{ asset('site/bootstrap.css') }}">
+
+    <title>{{ $title ?? 'Bruno Vieira | Full-Stack Developer' }}</title>
 </head>
-<body >
-   {{$slot}}
+<body>
+    {{ $slot }}
+
+    <!-- JS -->
+    <script src="{{ asset('site/jquery.js') }}"></script>
+    <script src="{{ asset('site/bootstrap.js') }}"></script>
 </body>
 </html>
-<script src="{{asset('site/jquery.js')}}"></script>
-<script src="{{asset('site/bootstrap.js')}}"></script>
